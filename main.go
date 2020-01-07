cd3746274d453b11cca7062c06256cc5c55bbb3e
NguyenNgocLam <lamnn8@fpt.com.vn>
2020-01-07 10:07:34 +0700

    delete fordel

--- main.go
+++ main.go
@@ -1,22 +1,2 @@
 package main
 
-const (
-	host = "localhost"
-	port= "5432"
-	user ="postgres"
-	password ="3414pic2ck2pi"
-	dbname="postgres"
-)
-
-//func main()  {
-//   db := driver.Connect(host,port,user,password,dbname)
-//   err := db.SQL.Ping()
-//   if err != nil {
-//
-//   }
-//
-//   fmt.Println("connection ok")
-//
-//
-//}
-
