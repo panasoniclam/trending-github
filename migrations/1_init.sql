1a33c9c9eb4f4bfc6c9ac07298a6a3e5e78613c2
NguyenNgocLam <lamnn8@fpt.com.vn>
2020-01-14 14:44:17 +0700

    update

--- migrations/1_init.sql
+++ migrations/1_init.sql
@@ -1,10 +1,11 @@
 -- +migrate Up
+-- +migrate StatementBegin
 CREATE TABLE "scc" (
-    id integer primary key,
-    username text unique ,
-    email text unique ,
-    password text
+    "id" integer primary key,
+    "username" text unique ,
+    "email" text unique ,
+    "password" text
 );
-
+-- +migrate StatementEnd
 -- +migrate Down
 DROP TABLE "scc"
